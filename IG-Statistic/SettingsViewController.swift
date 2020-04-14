657f1dd3a737bab4c6876d0e164314d8ab55082c
badyi <badyi7@icloud.com>
2020-04-14 23:24:12 +0300

    logout

--- IG-Statistic/SettingsViewController.swift
+++ IG-Statistic/SettingsViewController.swift
@@ -32,14 +32,5 @@ class SettingsViewController: UIViewController {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         appDelegate.window?.rootViewController = loginPage
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
