5a76e86c877ca98eb6f46ac226a61d94715327a0
badyi <badyi7@icloud.com>
2020-05-30 23:57:35 +0300

    fhh

---
+++ IG-Statistic/ViewControllers/testViewController.swift
@@ -0,0 +1,30 @@
+//
+//  testViewController.swift
+//  IG-Statistic
+//
+//  Created by Бадый Шагаалан on 21.03.2020.
+//  Copyright © 2020 Бадый Шагаалан. All rights reserved.
+//
+
+import UIKit
+
+class testViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
